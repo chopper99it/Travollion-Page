d003d61e0452ff5575f32dd766be475ca82a6fe9
tehv1007 <hanquoclythu@gmail.com>
2023-01-03 23:22:43 +0700

    first commit

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,5 +1,300 @@
 const Experiences = () => {
-  return <div>Experiences</div>;
+  return (
+    <section>
+      {/* Container */}
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+        {/* Layout */}
+        <div>
+          {/* Title */}
+          <div>
+            <h2 className="text-5xl md:text-[64px] leading-[64px] text-[#172432] mb-5">
+              Traveler’s Experiences
+            </h2>
+            <div className="border-[3px] border-[#FF7757] bg-[#FF7757] mb-8 w-[285px] md:w-[347px]"></div>
+            <p className="text-lg leading-[21px] md:text-2xl text-[#767E86]">
+              Here some awesome feedback from our travelers
+            </p>
+          </div>
+
+          {/* Items */}
+          <div className="grid md:grid-cols-3 gap-8">
+            {/* Item */}
+            <div>
+              <img
+                className="h-[100px] w-[100px] relative left-5 top-[35px] z-50"
+                src="https://diadiemhanquoc.com/wp-content/uploads/2023/01/Avatar3.png"
+                alt=""
+              />
+              <div className="flex flex-col items-start gap-5 px-5 pt-[60px] isolate bg-[#F5F6F7] rounded-xl">
+                <p className="text-[#767E86] text-lg leading-[23px]">
+                  But I must explain to you how all this mistaken idea of
+                  denouncing pleasure and praising pain was born and I will give
+                  you a complete account of the system and expound the actual
+                  teachings of the great explorer of the truth, the master-
+                  builder of human happiness.
+                </p>
+                <div className="flex gap-2">
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                </div>
+                <div className="text-[#767E86] mb-8 ">
+                  <h4 className="text-2xl">John Doe</h4>
+                  <p className="text-lg leading-[21px]">Accountant</p>
+                </div>
+              </div>
+            </div>
+            {/* Item */}
+            <div className="">
+              <img
+                className="h-[100px] w-[100px] relative left-5 top-[35px] z-50"
+                src="https://diadiemhanquoc.com/wp-content/uploads/2023/01/Avatar2.png"
+                alt=""
+              />
+              <div className="flex flex-col items-start gap-5 px-5 pt-[60px] isolate bg-[#F5F6F7] rounded-xl">
+                <p className="text-[#767E86] text-lg leading-[23px]">
+                  But I must explain to you how all this mistaken idea of
+                  denouncing pleasure and praising pain was born and I will give
+                  you a complete account of the system and expound the actual
+                  teachings of the great explorer of the truth, the master-
+                  builder of human happiness.
+                </p>
+                <div className="flex gap-2">
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                </div>
+                <div className="text-[#767E86] mb-8 ">
+                  <h4 className="text-2xl">John Doe</h4>
+                  <p className="text-lg leading-[21px]">Accountant</p>
+                </div>
+              </div>
+            </div>
+            {/* Item */}
+            <div className="">
+              <img
+                className="h-[100px] w-[100px] relative left-5 top-[35px] z-50"
+                src="https://diadiemhanquoc.com/wp-content/uploads/2023/01/Avatar.png"
+                alt=""
+              />
+              <div className="flex flex-col items-start gap-5 px-5 pt-[60px] isolate bg-[#F5F6F7] rounded-xl">
+                <p className="text-[#767E86] text-lg leading-[23px]">
+                  But I must explain to you how all this mistaken idea of
+                  denouncing pleasure and praising pain was born and I will give
+                  you a complete account of the system and expound the actual
+                  teachings of the great explorer of the truth, the master-
+                  builder of human happiness.
+                </p>
+                <div className="flex gap-2">
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M10 0.5L13.09 6.76L20 7.77L15 12.64L16.18 19.52L10 16.27L3.82 19.52L5 12.64L0 7.77L6.91 6.76L10 0.5Z"
+                      fill="#FFC107"
+                    />
+                  </svg>
+                </div>
+                <div className="text-[#767E86] mb-8 ">
+                  <h4 className="text-2xl">John Doe</h4>
+                  <p className="text-lg leading-[21px]">Accountant</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Slide button */}
+          <div className="flex justify-center gap-10 lg:justify-end mb-20 mt-[100px] md:mt-40 lg:mb-0">
+            <button className="flex flex-col items-start py-7 px-5 rounded-xl gap-1 bg-[#FF7757] h-[66px] w-[60px] ">
+              <svg
+                className="w-5 h-2.5"
+                width={12}
+                height={23}
+                viewBox="0 0 12 23"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M11 1.5L1 11.5L11 21.5"
+                  stroke="white"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+            <button className="flex flex-col items-start py-7 px-5 rounded-xl gap-1 bg-black h-[66px] w-[60px]">
+              <svg
+                className="w-5 h-2.5"
+                width={12}
+                height={23}
+                viewBox="0 0 12 23"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M1 21.5L11 11.5L0.999999 1.5"
+                  stroke="white"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experiences;
